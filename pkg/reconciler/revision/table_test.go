2b834e622b7c8e009c4cf207e3bb7983b135303c
Sascha Schwarze <schwarzs@de.ibm.com>
2023-09-18 15:36:10 +0200

    Use hashing to compare deployment specs for equality

--- pkg/reconciler/revision/table_test.go
+++ pkg/reconciler/revision/table_test.go
@@ -203,7 +203,7 @@ func TestReconcile(t *testing.T) {
 			Revision("foo", "fix-containers",
 				WithLogURL, allUnknownConditions, withDefaultContainerStatuses(), WithRevisionObservedGeneration(1)),
 			pa("foo", "fix-containers", WithReachabilityUnknown),
-			changeContainers(deploy(t, "foo", "fix-containers")),
+			changeContainers(t, deploy(t, "foo", "fix-containers")),
 			image("foo", "fix-containers"),
 		},
 		WantUpdates: []clientgotesting.UpdateActionImpl{{
@@ -222,7 +222,7 @@ func TestReconcile(t *testing.T) {
 				WithLogURL, allUnknownConditions,
 				withDefaultContainerStatuses(), WithRevisionObservedGeneration(1)),
 			pa("foo", "failure-update-deploy"),
-			changeContainers(deploy(t, "foo", "failure-update-deploy")),
+			changeContainers(t, deploy(t, "foo", "failure-update-deploy")),
 			image("foo", "failure-update-deploy"),
 		},
 		WantUpdates: []clientgotesting.UpdateActionImpl{{
@@ -688,7 +688,7 @@ func TestReconcile(t *testing.T) {
 		},
 		WantCreates: []runtime.Object{
 			pa("foo", "image-pull-secrets"),
-			deployImagePullSecrets(deploy(t, "foo", "image-pull-secrets"), "foo-secret"),
+			deployImagePullSecrets(t, deploy(t, "foo", "image-pull-secrets"), "foo-secret"),
 			imagePullSecrets(image("foo", "image-pull-secrets"), "foo-secret"),
 		},
 		WantStatusUpdates: []clientgotesting.UpdateActionImpl{{
@@ -823,6 +823,15 @@ func TestReconcile(t *testing.T) {
 	}))
 }
 
+func deploySetSpecHash(t *testing.T, deployment *appsv1.Deployment) *appsv1.Deployment {
+	err := resources.SetSpecHashAnnotation(deployment)
+	if err != nil {
+		t.Logf("Error on SetSpecHashAnnotation: %v", err)
+	}
+
+	return deployment
+}
+
 func readyDeploy(deploy *appsv1.Deployment) *appsv1.Deployment {
 	deploy.Status.Conditions = []appsv1.DeploymentCondition{{
 		Type:   appsv1.DeploymentProgressing,
@@ -856,11 +865,11 @@ func noOwner(deploy *appsv1.Deployment) *appsv1.Deployment {
 	return deploy
 }
 
-func deployImagePullSecrets(deploy *appsv1.Deployment, secretName string) *appsv1.Deployment {
+func deployImagePullSecrets(t *testing.T, deploy *appsv1.Deployment, secretName string) *appsv1.Deployment {
 	deploy.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{{
 		Name: secretName,
 	}}
-	return deploy
+	return deploySetSpecHash(t, deploy)
 }
 
 func imagePullSecrets(img *caching.Image, secretName string) *caching.Image {
@@ -870,12 +879,12 @@ func imagePullSecrets(img *caching.Image, secretName string) *caching.Image {
 	return img
 }
 
-func changeContainers(deploy *appsv1.Deployment) *appsv1.Deployment {
+func changeContainers(t *testing.T, deploy *appsv1.Deployment) *appsv1.Deployment {
 	podSpec := deploy.Spec.Template.Spec
 	for i := range podSpec.Containers {
 		podSpec.Containers[i].Image = "asdf"
 	}
-	return deploy
+	return deploySetSpecHash(t, deploy)
 }
 
 func withDefaultContainerStatuses() RevisionOption {
